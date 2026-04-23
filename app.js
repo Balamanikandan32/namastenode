a973ce1e53da39f49d70440daafe554636613ea6
Balamanikandan <bala34510@gmail.com>
2026-04-23 07:50:55 +0530

    First node js code

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = "bala";
+
+let num1 = 10,
+  num2 = 20;
+
+let result = num1 + num2;
+
+console.log("Name is", name);
+console.log(`The sum of two number is  ${result}`);
+
+console.log(globalThis);
