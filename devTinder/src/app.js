509e5ec44dd4f842cca552301174807313353ecc
Balamanikandan <bala34510@gmail.com>
2026-05-02 21:35:46 +0530

    Ep-7 get data from database using moongose api's

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -8,22 +8,60 @@ const User = require("./model/user");
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// create a new user
 app.post("/signup", async (req, res) => {
   // Creating a new instance of User model
-  const newUser = new User({
-    firstName: "Bala",
-    lastName: "manikandan",
-    age: 24,
-    email: "balabala@gamil.com",
-    password: "password",
-  });
-
+  const newUser = new User(req.body);
+  // Either use try -catch or use error handling middleware to handle errors
   try {
     await newUser.save();
     res.send("User saved successfully");
   } catch (err) {
     console.log("Error saving user:", err);
-    res.status(500).send("Erro saving user");
+    res.status(500).send("Error saving user");
+  }
+});
+
+// get user details by email id
+app.get("/user/:email", async (req, res) => {
+  const emailId = req.params.email;
+  try {
+    const user = await User.find({ email: emailId });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.json(user);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//get user details by id
+app.get("/user/id/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId);
+    res.json(user);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//get all the users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.json(users);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong");
   }
 });
 
