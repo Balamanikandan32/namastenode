183005db36a6cfc2e8b6941399803ab983a4aaef
Balamanikandan <bala34510@gmail.com>
2026-05-15 13:09:53 +0530

    Ep-14 develop feed api and pagination

--- devTinder/src/routes/user.js
+++ devTinder/src/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authMiddleware = require("../middleware/auth-middleware");
 const ConnectionRequestModel = require("../model/connectionRequest");
+const User = require("../model/user");
 
 const userRouter = express.Router();
 
-const POPULATE_DATA = ["firstName", "lastName", "photoUrl", "skills"];
+const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "skills"];
 
 userRouter.get("/user/connectionRequests", authMiddleware, async (req, res) => {
   try {
@@ -12,7 +13,7 @@ userRouter.get("/user/connectionRequests", authMiddleware, async (req, res) => {
     const connectionRequest = await ConnectionRequestModel.find({
       status: "interested",
       toUserId: loggedInUser._id,
-    }).populate("fromUserId", POPULATE_DATA);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.send({
       message: "Pending connection request",
@@ -33,8 +34,8 @@ userRouter.get("/user/connections", authMiddleware, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
       ],
     })
-      .populate("fromUserId", POPULATE_DATA)
-      .populate("toUserId", POPULATE_DATA);
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
     const data = connections.map((item) => {
       if (loggedInUser._id.toString() === item.fromUserId._id.toString()) {
@@ -49,4 +50,61 @@ userRouter.get("/user/connections", authMiddleware, async (req, res) => {
   }
 });
 
+userRouter.get("/user/feed", authMiddleware, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // PAGINATION
+    const pageNumber = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    // LIMIT THE NUMBER OF DOCUEMENT
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (pageNumber - 1) * limit;
+
+    // LoggedInuser can see all the users Except
+    // 1) Their own card(card means profile)
+    // 2) Their connections
+    // 3) Ignored People
+    // 4) Already send connection request
+    // To simplify the loggedInUser feed does not include their own card and already seen user(who marked as ignored or interested or accepted or rejected)
+
+    // Get all the connection Request(ignored, interested, accepted, rejected) of loggedInUser
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    // These are the user who are hidden from loggedInUser feed.
+    const hiddenUserFromFeed = new Set();
+    connectionRequest.map((request) =>
+      hiddenUserFromFeed
+        .add(request.fromUserId.toString())
+        .add(request.toUserId.toString()),
+    );
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
+  }
+});
+
 module.exports = userRouter;
+
+// PAGINATION
+
+//    /user/feed?page=1&limit=10   skip(0)  limit(10)
+//    /user/feed?page=2&limit=10   skip(10) limit(10)
+//    /user/feed?page=3&limit=10   skip(20) limit(10)
+
+// skip = (page -1) * limit
